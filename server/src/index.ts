6bc90960039ad146168e0ea317be3dcb3d690f09
jihed-tatouh <jihedtatouh@tutanota.com>
2021-12-21 12:35:46 +0100

    feat: setup `socket.io` server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,22 @@
 import express from "express";
 import { createServer } from "http";
+import { Server } from "socket.io";
+
 const app = express();
 const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: { origin: "*", methods: ["GET", "POST"] },
+});
+io.on("connection", (socket) => {
+  socket.on("msg", (msg) => {
+    console.log("msg from client:", msg);
+  });
+  console.log("user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
 httpServer.listen(3000, async () => {
   console.log("Server running on port 3000");
 });
