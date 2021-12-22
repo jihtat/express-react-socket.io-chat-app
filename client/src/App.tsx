16a21a0fc637b1f3b602f328223ec5aae2381f3e
jihed-tatouh <jihedtatouh@tutanota.com>
2021-12-22 16:27:40 +0100

    feat: add `send messages to rooms` logic in `app` component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,45 @@
-import { useEffect } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { io } from "socket.io-client";
 import "./App.css";
+import Message from "./components/Msesage";
+import Room from "./components/Room";
+import Conversation from "./components/Conversation";
 
 const socket = io("localhost:3000");
 function App() {
-  useEffect(() => {}, []);
-  function sendmsg() {
-    const msg = "hello";
-    socket.emit("msg", msg);
+  const [messages, addMessage] = useState<string[]>([]);
+  const [room, setRoom] = useState("public");
+  useEffect(() => {
+    socket.on("connect", () => {
+      addMessage([
+        ...messages,
+        `You are connected with id ${socket.id} in room ${room}`,
+      ]);
+    });
+  }, []);
+  useEffect(() => {
+    socket.emit("join-room", room, (msg: string) => {
+      addMessage([msg]);
+    });
+  }, [room]);
+  socket.on("display-msg", (msg) => {
+    addMessage([...messages, msg]);
+  });
+  function sendmsg(msg: string) {
+    socket.emit("msg", msg, room);
   }
   return (
-    <div>
-      <button onClick={sendmsg}>send msg</button>
-    </div>
+    <Fragment>
+      <div className="container d-flex flex-column justify-content-center">
+        <Conversation messages={messages} />
+        <Message
+          messages={messages}
+          addMessage={addMessage}
+          sendmsg={sendmsg}
+        />
+        <Room setRoom={setRoom} />
+      </div>
+    </Fragment>
   );
 }
 
