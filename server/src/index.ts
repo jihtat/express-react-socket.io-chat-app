5f434714d828be763e7e2840149841ab220d1750
jihed-tatouh <jihedtatouh@tutanota.com>
2021-12-22 16:22:37 +0100

    feat: add `send messages to rooms` logic in `server index` file

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,10 +8,20 @@ const io = new Server(httpServer, {
   cors: { origin: "*", methods: ["GET", "POST"] },
 });
 io.on("connection", (socket) => {
-  socket.on("msg", (msg) => {
-    console.log("msg from client:", msg);
+  socket.on("msg", (msg, room) => {
+    if (room === "public") {
+      socket.broadcast.emit("display-msg", msg);
+    } else {
+      socket.to(room).emit("display-msg", msg);
+    }
+  });
+  socket.on("join-room", (room, addMessage) => {
+    socket.join(room);
+    addMessage(`You joined the room ${room}`);
+    socket.to(room).emit("display-msg", `${socket.id} joined the room ${room}`);
   });
   console.log("user connected");
+
   socket.on("disconnect", () => {
     console.log("user disconnected");
   });
